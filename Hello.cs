c904dced6b6430161b6f61c2af1809764a7c3063
BB23-ZA <40992727+BB23-ZA@users.noreply.github.com>
2022-08-31 16:40:50 +0200

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!"); 
+      Console.WriteLine("It's nice to meet you in C#")
+      Console.WriteLine("It's important to practice as much as possible")   
+    }
+  }
+}
